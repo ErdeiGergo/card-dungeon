0f40932858b6bc33bcd3e7d4d22789287ae285a5
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2025-10-16 13:54:33 +0200

    Megjelenítés

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import Card from './components/Card'
+
+export type Enemy = {
+  enemyName: string,
+  enemyIcon: string,
+  level: number,
+  reward: string,
+  penalty: string
+}
+
+async function getCards() {
+  try{
+    const res = await fetch(`cards.json`)
+    return res.json()
+  }catch{
+    console.error("Hiba")
+    return[]
+  }
+}
+
 const App = () => {
+  const [cards, setCards] = useState<Enemy[]>()
+
+  useEffect(() => {
+    getCards().then(data => 
+    setCards(data)
+    )
+  }, [])
+
+  
+  useEffect(() => {
+    if (cards) {
+      setCards([...cards].sort(() => Math.random() - 0.5));
+    }
+  }, []);
+
+
   return (
-    <div>App</div>
+    <main>
+      <div className='cardList'>
+        {cards?.map(card => <Card {...card}/>)}
+      </div>
+    </main> 
   )
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import type { Enemy } from "../App"
 
-const Card = () => {
+const Card = (props: Enemy) => {
+
   return (
-    <div>Card</div>
+    <div className="card">
+      <h1>{props.enemyName}</h1>
+      <h2>{props.enemyIcon}</h2>
+      <h3>level {props.level}</h3>
+      <h5>Reward: {props.reward}</h5>
+      <h5>Penalty: {props.penalty}</h5>
+    </div>
   )
 }
 
