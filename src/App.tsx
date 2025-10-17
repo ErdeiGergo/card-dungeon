6085932f2cab4dfecd474f7eff02801b9703a376
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2025-10-17 17:52:34 +0200

    flee és pick function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Card from './components/Card'
 
 export type Enemy = {
@@ -20,27 +20,74 @@ async function getCards() {
 }
 
 const App = () => {
-  const [cards, setCards] = useState<Enemy[]>()
+  const [cards, setCards] = useState<Enemy[]>([])
+  const [fleeList, setFleeList] = useState<Enemy[]>([])
 
+  const [health, setHealth] = useState<String>("❤️❤️❤️❤️❤️")
+  const [strength, setStrength] = useState<number>(1)
+
+  const [currentCard, setCurrentCard] = useState<Enemy>(cards[0])
+  
   useEffect(() => {
     getCards().then(data => 
     setCards(data)
     )
   }, [])
 
-  
-  useEffect(() => {
-    if (cards) {
-      setCards([...cards].sort(() => Math.random() - 0.5));
-    }
-  }, []);
+  const pick = () => {
+    if(!cards || currentCard) return
+    const random = Math.floor(Math.random() * cards.length)
+    const card = cards[random]
+    setCurrentCard(card)
+    setCards(prev => prev.filter(e => e != card))
+  }
 
+  function visszatesz(card: Enemy){
+    setCards(prev => [...prev, card])
+  }
+
+  const fight = () => {
+    if(!currentCard)return
+    const damage = Math.floor(Math.random() * 6) + 1 + strength
+    alert(damage)
+    if (damage > currentCard?.level)
+      alert("Nyertél")
+    else
+      alert("Vesztettél")
+  }
+
+  const flee = (card: Enemy) => {
+    if (fleeList.length < 2){
+      setFleeList(prev => [...prev, card])
+      pick()
+    }
+  }
 
   return (
     <main>
-      <div className='cardList'>
-        {cards?.map(card => <Card {...card}/>)}
-      </div>
+      <section>
+        <h1>Max 2 talonban</h1>
+        <div className='talon'>
+          {fleeList.map(c => <Card {...c}/>)} 
+        </div>
+      </section>
+      <section className='battleField'>
+        {currentCard && <Card {...currentCard} />}
+        <button id='dice'>
+          🎲
+        </button>
+        <button id='fleeBtn' onClick={() => flee(currentCard)}>
+          Flee👣
+        </button>
+      </section>
+      <section className='stats'>
+        <div>
+          <h1>Max 5</h1>
+          <p>Health: {health}</p>
+          <p>Strength: {strength}</p>
+        </div>
+        <div id='deck' onClick={()=>pick()}>{cards?.length}</div>
+      </section>
     </main> 
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,19 @@ import type { Enemy } from "../App"
 const Card = (props: Enemy) => {
 
   return (
-    <div className="card">
-      <h1>{props.enemyName}</h1>
-      <h2>{props.enemyIcon}</h2>
-      <h3>level {props.level}</h3>
-      <h5>Reward: {props.reward}</h5>
-      <h5>Penalty: {props.penalty}</h5>
+    <div>
+
+      <div className="card">
+        <h1>{props.enemyName}</h1>
+        <h2>{props.enemyIcon}</h2>
+        <h3>level {props.level}</h3>
+        <h5>Reward: {props.reward}</h5>
+        <h5>Penalty: {props.penalty}</h5>
+      </div>
+
+      <button className='fightBtn'>
+        Fight⚔️
+      </button>
     </div>
   )
 }
